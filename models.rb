6c8f5abe41d012176d24279a281757ac5c4f9f6d
Andrew <zak.31337@gmail.com>
2009-11-07 16:08:36 +0500

    Много чего, долго не коммител

--- models.rb
+++ models.rb
@@ -1,6 +1,7 @@
 require 'rubygems' 
 require 'dm-core'
 require 'usersystem'
+require 'md5'
 
 class DateTime
   def rfc822
@@ -13,14 +14,17 @@ class DateTime
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:///#{Dir.pwd}/../rpoint.db")
+#DataMapper::Logger.new('dm.log', :debug, "\n", true)
+DataMapper::Logger.new(STDOUT, 0)
+DataObjects::Sqlite3.logger = DataObjects::Logger.new(STDOUT, 0)
 
 class User < UserSystem::BasisUser
   include DataMapper::Resource
 
   
   property :id, Serial
-  property :login, String, :length => 0..255
-  property :email, String, :length => 0..255
+  property :login, String, :length => 0..255, :unique => true, :key => true
+  property :email, String, :nullable => false, :unique => true, :format => :email_address, :key => true
   property :password, String, :length => 0..255
   property :state, String, :length => 0..255
   property :token, String, :length => 0..255
@@ -28,17 +32,26 @@ class User < UserSystem::BasisUser
   property :created_at, DateTime
   property :updated_at, DateTime
   
-  has n, :courses
+  has n, :owncourses, :model => 'Course'
   has n, :fieldworks
   has n, :invites
   has n, :sessions
+  has n, :comments
   
   has n, :permits
   has n, :permissions, :through => :permits
   
-  #before :save do 
-  #  self.password = UserSystem.encrypt(self.password) if self.id.nil?
-  #end
+  has n, :subcourses
+  has n, :courses, :through => :subcourses
+  
+  has n, :sublectures
+  has n, :lectures, :through => :sublectures
+  
+  belongs_to :invite
+  
+  def avatar
+    "http://www.gravatar.com/avatar/#{MD5::md5(email.downcase)}?s=30"
+  end
 end
 
 class Permit
@@ -71,6 +84,19 @@ class Session
   property :referer, String, :length => 0..255
   
   belongs_to :user
+  
+  has n, :notifications
+end
+
+class Notification
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :message, String, :length => 0..255
+  property :read, Boolean, :default => false
+  property :created_at, DateTime
+  
+  belongs_to :session
 end
 
 class Invite
@@ -79,10 +105,11 @@ class Invite
   property :id, Serial
   property :token, String, :length => 0..255
   property :value, Integer, :default  => 1
-  property :expires_at, DateTime
+  property :expired_at, DateTime
   property :created_at, DateTime
   
   belongs_to :user
+  has n, :users
 end
 
 class Course
@@ -100,6 +127,18 @@ class Course
   belongs_to :user
   
   has n, :lectures
+  
+  has n, :subcourses
+  has n, :users, :through => :subcourses
+end
+
+class Subcourse
+  include DataMapper::Resource
+  
+  property :id, Serial
+  
+  belongs_to :course
+  belongs_to :user
 end
 
 class Lecture
@@ -108,6 +147,7 @@ class Lecture
   property :id, Serial
   property :number, Integer 
   property :subject, String, :length => 0..255
+  property :state, String, :length => 0..255
   property :content, Text
   property :fieldwork, Text
   property :created_at, DateTime
@@ -117,6 +157,22 @@ class Lecture
   
   has n, :theses
   has n, :fieldworks
+  
+  has n, :sublectures
+  has n, :users, :through => :sublectures
+  
+  def next
+    Lecture.first(:course_id => self.course_id, :number.gt => self.number, :order => [:number.asc])
+  end
+end
+
+class Sublecture
+  include DataMapper::Resource
+  
+  property :id, Serial
+  
+  belongs_to :lecture
+  belongs_to :user
 end
 
 class Thesis
@@ -155,28 +211,44 @@ class Appraisal
   belongs_to :fieldwork
 end
 
+class Comment
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :page, String, :length => 0..255
+  property :comment, Text
+  property :created_at, DateTime
+
+  belongs_to :user
+end
+
 def install
   DataMapper.auto_migrate!
   
-  User.new(:login => 'admin', :password => '7c4a8d09ca3762af61e59520943dc26494f8941b').save!
-  user = User.new(:login => 'zak', :password => '0f0109c6a2ee3e044aa6be74f92995daa86949bf')
+  User.new(:login => 'admin', :password => '7c4a8d09ca3762af61e59520943dc26494f8941b', :email => 'admin@rpoint.ru', :invite_id => 1, :created_at => Time.now).save!
+  user = User.new(:login => 'zak', :password => '0f0109c6a2ee3e044aa6be74f92995daa86949bf', :email => 'zak@rpoint.ru', :invite_id => 1, :created_at => Time.now)
   user.save!
   { :course_add => 'Добавить новый курс', :course_del => 'Удалить курс', :course_edit => 'Редактировать курс',
     :lecture_add => 'Добавить лекцию',  :lecture_edit => 'Редактировать лекцию', :lecture_del => 'Удалить лекцию',
     :thesis_add => 'Добавить тезис', :thesis_del => 'Удалить тезис', :thesis_edit => 'Редактировать тезис',
     :appraisal => 'Поставить оценку',
     :permit => 'Раздача привелегий',
-    :permissions_view => 'Просмотр привелегий', :permissions_add => 'Добавление привелегий', :permissions_del => 'Удаление привелегий',
-    :permissions_edit => 'Редактирование привелегий'}.find_all do |event, description|
+    :permissions_view => 'Просмотр привелегий', :permissions_add => 'Добавление привелегий', :permissions_del => 'Удаление привелегий', :permissions_edit => 'Редактирование привелегий',
+    :invite_add => 'Создать инвайт'}.find_all do |event, description|
     p = Permission.new(:event => event, :description => description)
     p.save!
     user.permissions << p
     user.save!
   end
   
-  Course.new(:title => 'Тестовый', :permalink => 'test', :description => 'Не очень длинный текст, а хотелось больше!', :user_id => 1, :created_at => Time.now).save!
-  Invite.new(:token => '123456789', :created_at => Time.now, :expires_at => (Time.now + 3600), :user_id => 1).save!
-  Invite.new(:token => '123', :created_at => (Time.now - 3600), :expires_at => (Time.now), :user_id => 1).save!
-  Lecture.new(:number => 1, :subject => 'Первая лекция', :content => 'Сама теория припрвленная <b>кодом</b>', :fieldwork => 'Практическое задание', :course_id => 1, :created_at => Time.now).save!
-  Lecture.new(:number => 2, :subject => 'Вторая лекция', :content => 'Сама теория припрвленная <b>кодом</b>', :fieldwork => 'Практическое задание', :course_id => 1, :created_at => Time.now).save!
+ # user.owncourses.new(:title => 'Тестовый', :permalink => 'test', :description => 'Не очень длинный текст, а хотелось больше!', :state => 'public', :created_at => Time.now).save!
+  user.invites.new(:token => '123456789', :created_at => Time.now, :value => 0, :expired_at => (Time.now + 3600)).save!
+ # user.invites.new(:token => '123', :value => 0, :created_at => (Time.now - 3600), :expired_at => (Time.now)).save!
+ # Lecture.new(:number => 1, :subject => 'Первая лекция', :content => 'Сама теория припрвленная <b>кодом</b>', :fieldwork => 'Практическое задание', :state => 'public', :course_id => 1, :created_at => Time.now).save!
+ # Lecture.new(:number => 2, :subject => 'Вторая лекция', :content => 'Сама теория припрвленная <b>кодом</b>', :fieldwork => 'Практическое задание', :state => 'public', :course_id => 1, :created_at => Time.now + 3600).save!
+ # Lecture.new(:number => 3, :subject => 'Третья лекция', :content => 'Сама теория припрвленная <b>кодом</b>', :fieldwork => 'Практическое задание', :state => 'public', :course_id => 1, :created_at => Time.now + 7200).save!
+ # Thesis.new(:content => 'Форматирование кода', :appraisal => "1|2|3|4|5", :lecture_id => 1).save!
+ # Thesis.new(:content => 'Семантика', :appraisal => "1|2|3|4|5", :lecture_id => 1).save!
+ # Thesis.new(:content => 'Реализация таблицы', :appraisal => "1|2|3|4|5", :lecture_id => 2).save!
+ # Thesis.new(:content => 'Оценка', :appraisal => "1|2|3|4|5", :lecture_id => 2).save!
 end
--- rpoint.rb
+++ rpoint.rb
@@ -28,8 +28,8 @@ helpers do
   end
   
   def authenticat_by_cookies
-    session = Session.first(:token => request.cookies["token"]) unless request.cookies["token"].nil? || request.cookies["token"].empty?
-    @current_user = session.user unless session.nil?
+    @session = Session.first(:token => request.cookies["token"]) unless request.cookies["token"].nil? || request.cookies["token"].empty?
+    @current_user = @session.user unless @session.nil?
   end
   
   def authenticat_by_password(user, password)
@@ -40,15 +40,16 @@ helpers do
   end
   
   def session_start!
-    Session.new(:token => UserSystem.random_string(14), :expires_at => Time.now + 21600, :ip => request.ip, :referer => request.referer, :user_id => @current_user.id).save!
+    @session = @current_user.sessions.new(:token => UserSystem.random_string(14), :expires_at => Time.now + 21600, :ip => request.ip, :referer => request.referer, :user_id => @current_user.id)
+    @session.save!
   end
    
   def tokeniz!
-    set_cookie("token", :value => @current_user.sessions.first.token, :expires => Time.now + 21600)
+    set_cookie("token", :value => @session.token, :expires => Time.now + 21600)
   end
   
   def logout
-    @current_user.sessions.destroy if @current_user
+    @session.destroy if @current_user
     delete_cookie("token")
   end
   
@@ -64,18 +65,45 @@ helpers do
     @current_user.accessed?(event)
   end
   
+  def comments
+    haml :comments, :layout => false
+  end
+  
+  def escape_javascript(javascript)
+    if javascript
+      javascript.gsub(/(on.*=["'].*["']|<script>.*<\/script>)/) { '' }
+    else
+      ''
+    end
+  end
+  
+  def notification(message)
+    @session.notifications.new(:message => message, :created_at => Time.now, :read => false).save!
+  end
+  
+  def notifications
+    return [] if @session.nil?
+    @notifications ||= @session.notifications.all(:read => false).each {|n| n.read = true; n.save!}
+  end
+  
+  def notifications_all
+    @session.notifications.all(:order => [:created_at.desc], :limit => 10)
+  end
+  
+  def access_course?(course)
+    @current_user.courses.include?(course) || course.user == @current_user || (!(p = Permission.first(:event => "course_edit_#{course.id}")).nil? && p.users.include?(@current_user))
+  end
+  
+  def access_lecture?(lecture)
+    @current_user.lectures.include?(lecture) || accessed?("lecture_edit")
+  end
+  
 end
 
 before do
   access_page = ['/signup', '/accessdenied', '/about', '/login', '/404']
   redirect '/login' unless logged_in? || access_page.include?(request.path)
   @body_class = ''
-
-  @test = request.path_info
-  @path = request.path
-  @query = request.query_string
-  @referer = request.referer 
-  @media = request.media_type
 end
 
 get '/about' do
@@ -86,9 +114,22 @@ get '/accessdenied' do
   haml :accessdenied
 end
 
+get '/accessdeniedcourse' do
+  haml :accessdeniedcourse
+end
+
+get '/accessdeniedlecture' do
+  haml :accessdeniedlecture
+end
+
 get '/404' do
   status 404
   haml :'404'
+end
+
+get '/index' do
+  @comments = Comment.all(:page => request.path)
+  haml :index
 end
 #----------------------------
 # Регистрация и аутентификация
@@ -102,7 +143,7 @@ post '/signup' do
   invite = Invite.first(:token => params[:invite])
   if invite.nil?
     @message = 'Пропуск ваш, не того образца.'
-  elsif invite.expires_at.to_time < Time.now
+  elsif invite.expired_at.to_time < Time.now
     @message = 'Фу! Да он протух! Видете плесень на уголке!?'
   elsif invite.value < 1
     @message = 'В вашем пропуске дырочка лишняя'
@@ -110,7 +151,7 @@ post '/signup' do
     invite.value -= 1
     invite.save!
     password = UserSystem.random_string(8)
-    user = User.new(:login => params[:login], :email => params[:email], :password => UserSystem.encrypt(password))
+    user = invite.users.new(:login => params[:login], :email => params[:email], :password => UserSystem.encrypt(password))
     user.save!
     @message = 'Ваш пасс - ' + password + ' И поменять его вы не сможете ))'
   end
@@ -123,6 +164,7 @@ end
 
 post '/login' do
   if authenticat_by_password(params[:login], params[:password])
+    notification 'Вход успешно осуществлен'
     redirect '/'
   else
     @message = 'Логин/паксс не верен'
@@ -148,7 +190,7 @@ end
 
 post '/permissions' do
   authorized?('permissions_add')
-  Permission.new(params).save!
+  Permission.new(:event => params[:event], :description => params[:description]).save! ? notification("Привелегия #{params[:event]} добавленна") : notification('Произошла какаето херня и привелегия не добавленна')
   redirect '/permissions'
 end
 
@@ -170,7 +212,7 @@ end
 
 get '/permissions/:permission/del' do
   authorized?('permissions_del')
-  Permission.first(:event => params[:permission]).destroy
+  Permission.first(:event => params[:permission]).destroy ? notification("Привелегия #{params[:permission]} удалина") : notification("Не удалось удалить #{params[:permission]}")
   redirect '/permissions'
 end
 
@@ -180,10 +222,11 @@ get '/permissions/:permission/edit' do
   haml :'permissions/edit'
 end
 
+# update permission
 post '/permissions/:permission' do
   authorized?('permissions_edit')
   @permission = Permission.first(:event => params[:permission])
-  @permission.update_attributes(:event => params[:event], :description => params[:description])
+  @permission.update_attributes(:event => params[:event], :description => params[:description]) ? notification("Привелегия #{params[:permission]} изменина") : notification("Не удалось изменить привелегию #{params[:permission]}")
   redirect 'permissions/' + @permission.event
 end
 
@@ -208,8 +251,34 @@ get '/user/:login' do
   haml :'users/show'
 end
 
+get '/dashbord' do
+  haml :'users/dashbord'
+end
+
+post '/dashbord/pass' do
+  @current_user.password = UserSystem.encrypt(params[:password]) if params[:password] == params[:confirm] && @current_user.password == UserSystem.encrypt(params[:old_pass])
+  @current_user.save! ? notification("Пароль изменен") : notification("Старый пароль еще в силе!")
+  redirect '/dashbord'
+end
+
+# create invites
+post '/dashbord/invite' do
+  authorized?('invite_add')
+  @current_user.invites.new(:value => params[:value], :expired_at => Time.now + params[:expired].to_f * 3600 * 24, :created_at => Time.now, :token => UserSystem.random_string(12)).save! ? notification("Пирожки готовы"): notification("Херь какаето, попробуй еще")
+  redirect '/dashbord'
+end
+
 error UserNotFound do
   haml :not_found
+end
+
+#-------------------
+# Комментарии
+#-------------------
+
+post '*/comment' do
+  Comment.new(:page => params["splat"], :comment => escape_javascript(params[:comment]), :user => @current_user, :created_at => Time.now).save! ? notification("Ты испачкал страницу #{params["splat"]}") : notification("Господь не дал опорочить твоими речами эту страницу!")
+  redirect params["splat"]
 end
 
 #-------------------
@@ -233,23 +302,38 @@ end
 # create course
 post '/course' do
   authorized?('course_add')
-  course = Course.new(params.merge(:created_at => Time.now, :user => @current_user)) #refactored it
-  course.save!
-  redirect course.permalink
+  course = @current_user.owncourses.new(:permalink => params[:permalink], :description => params[:description], :title => params[:title], :created_at => Time.now) 
+  course.save! ? notification('Курс добавлен') : notification('Мы не справились с этим запросом')
+  redirect params[:permalink]
 end
 
 # read course
 get '/:course' do
   @course = Course.first(:permalink => params[:course])
+  redirect '/accessdeniedcourse' unless access_course?(@course)
+  @comments = Comment.all(:page => request.path)
   haml :'courses/show'
+end
+
+get '/:course/buy' do
+  course = Course.first(:permalink => params[:course])
+  @current_user.courses << course
+  @current_user.lectures << course.lectures.first(:order => [:number.asc])
+  @current_user.save! ? notification("Вы подписались на курс #{course.title}") : notification('Хм, вам не удалось подписаться на курс')
+  redirect '/'+params[:course]
+end
+
+get '/:course/edit' do
+  authorized?('course_edit')
+  @course = Course.first(:permalink => params[:course])
+  haml :'courses/edit'
 end
 
 # update course
 post '/:course' do
   authorized?('course_edit')
   course = Course.first(:permalink => params[:course])
-  course.update_attributes = params.merge(:updated_at => Time.now) #refactored it
-  course.save!
+  course.update_attributes(:permalink => params[:permalink], :description => params[:description], :title => params[:title], :updated_at => Time.now) ? notification("Изменения '#{params[:title]}' прошли успешно") : notification('Курс не поддался')
   redirect course.permalink
 end
 
@@ -257,7 +341,7 @@ end
 # delete course
 get '/:course/del' do
   authorized?('course_del')
-  Course.first(:permalink => params[:course]).destroy
+  Course.first(:permalink => params[:course]).destroy ? notification("Ты только что пришиб #{params[:course]}") : notification("Он сопративляется!")
   redirect '/'
 end
 
@@ -277,7 +361,7 @@ post '/:course/lecture' do
   authorized?('lecture_add')
   @course = Course.first(:permalink => params[:course])
   lecture = Lecture.new(:course => @course, :subject => params[:subject], :number => params[:number], :content => params[:content], :fieldwork => params[:fieldwork], :created_at => Time.now)
-  lecture.save!
+  lecture.save! ? notification("Отличная лекция #{params[:subject]}") : notification('Что то пошло не так и мы потеряли сей труд')
   redirect '/' + @course.permalink
 end
 
@@ -285,14 +369,34 @@ end
 get '/:course/:lecture' do
   @course = Course.first(:permalink => params[:course])
   @lecture = @course.lectures.first(:number => params[:lecture])
+  redirect '/accessdeniedlecture' unless access_lecture?(@lecture)
   @fieldworks = @lecture.fieldworks(:user => @current_user)
+  @comments = Comment.all(:page => request.path)
   haml :'lectures/show'
+end
+
+get '/:course/:lecture/del' do
+  authorized?('lecture_del')
+  course = Course.first(:permalink => params[:course])
+  lecture = course.lectures.first(:number => params[:lecture])
+  lecture.destroy ? notification('Все ее больше нет ((') : notification('Is a live!!!')
+  redirect "/#{params[:course]}"
+end
+
+get '/:course/:lecture/edit' do
+  authorized?('lecture_edit')
+  @course = Course.first(:permalink => params[:course])
+  @lecture = @course.lectures.first(:number => params[:lecture])
+  haml :'lectures/edit'
 end
 
 # update lecture
 post '/:course/:lecture' do
-  @course = Course.first(:permalink => params[:course])
-  @lecture = @course.lectures.first(:number => params[:lecture])
+  authorized?('lecture_edit')
+  course = Course.first(:permalink => params[:course])
+  lecture = course.lectures.first(:number => params[:lecture])
+  lecture.update_attributes(:number => params[:number], :subject => params[:subject], :content => params[:content], :fieldwork => params[:fieldwork], :updated_at => Time.now) ? notification("В #{params[:subject]} что то поменялось") : notification('Изменения не внесены')
+  redirect "/#{params[:course]}/#{lecture.number}"
 end
 
 # creat thesis
@@ -301,7 +405,7 @@ post '/:course/:lecture/thesis' do
   authorized?("thesis_add")
   @course = Course.first(:permalink => params[:course])
   @lecture = @course.lectures.first(:number => params[:lecture])
-  @lecture.theses.new(:content => params[:content], :appraisal => params[:appraisal]).save!
+  @lecture.theses.new(:content => params[:content], :appraisal => params[:appraisal]).save! ? notification('Тэзис добавлен') : notification('Он не лезет!')
   redirect "/#{params[:course]}/#{params[:lecture]}"
 end
 
@@ -313,17 +417,18 @@ post '/:course/:lecture/fieldwork' do
   output_file = "#{course.permalink}-#{@current_user.login}-#{file_params[:filename]}"
   output_file = File.open('./public/files/'+output_file) { "#{course.permalink}-#{@current_user.login}-#{UserSystem.random_string(4)}-#{file_params[:filename]}" } rescue output_file
   FileUtils.mv file_params[:tempfile].path, './public/files/'+output_file
-  lecture.fieldworks.new(:description => params[:description], :attach => output_file, :user_id => @current_user.id, :created_at => Time.now).save!
+  lecture.fieldworks.new(:description => params[:description], :attach => output_file, :user => @current_user, :created_at => Time.now).save! ? notification('Уверен, что это стоит показывать?') : notification('Придется поработать еще')
   redirect '/' + params[:course] + '/' + params[:lecture]
 end
 
 # appraisal fieldwork
 get '/:course/:lecture/:user' do
-  authorized?('appraisal')
+  authorized?('appraisal') if @current_user.login != params[:user]
   @course = Course.first(:permalink => params[:course])
   @lecture = @course.lectures.first(:number => params[:lecture])
   @user = User.first(:login => params[:user])
   @fieldworks = @lecture.fieldworks(:user => @user)
+  @comments = Comment.all(:page => request.path)
   haml :'lectures/appraisal'
 end
 
@@ -335,12 +440,22 @@ post '/:course/:lecture/:user' do
     thesis = Thesis.first(:id => thesis_id)
     fieldwork.appraisals.new(:thesis => thesis, :mark => mark).save!
   end
+  notification('Этим двоечникам явно не повезло')
   redirect "/#{params[:course]}/#{params[:lecture]}/#{params[:user]}"
 end
 
+get '/:course/:lecture/:user/next' do
+  authorized?('appraisal')
+  @course = Course.first(:permalink => params[:course])
+  @lecture = @course.lectures.first(:number => params[:lecture])
+  @user = User.first(:login => params[:user])
+  @user.lectures << @lecture.next unless @lecture.next.nil?
+  @user.save
+  redirect "/#{params[:course]}/#{params[:lecture]}"
+end
+
 get '/' do
-  @user = User.first.login
-  haml :index
+  redirect '/index'
 end
 
 error NoMethodError do
